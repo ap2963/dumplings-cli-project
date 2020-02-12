57887d67f1f822165b1f2b4bfb90202fd1abebca
agnespark824 <agnespark824@gmail.com>
2020-02-12 03:38:07 -0500

    Methods instantiate Country and Dumpling instances by name.

--- dumplings-cli-project/lib/scraper.rb
+++ dumplings-cli-project/lib/scraper.rb
@@ -3,37 +3,46 @@ require 'open-uri'
 
 class Scraper 
     
-    def get_page 
+    def get_article 
         doc = Nokogiri::HTML(open('https://thecitylane.com/the-best-65-dumplings-around-the-world/'))
-    end
-
-    def get_article
-        self.get_page.css(".vs-post-content clearfix")
+        article = doc.css(".vw-post-content")
+        article
     end
 
     def get_pair
-        self.get_article.css("h4").text
-    end
-
-    def get_country
-        self.get_pair each do |p|
-            country.name = p.split(" - ")[1]
+        pair_array = []
+        self.get_article.css("h4").each do |p|
+            pair_array << p.to_s[4...-6]
+        end
+        pair_array.map do |p|
+            p.split(" \u2013 ")
         end
     end
 
     def make_country
-        self.get_country.each do |vs|
+        self.get_pair.each do |p|
+            country_name = p[1]
+            country = Country.new(country_name) #if it doesn't already exist
+        end
     end
+        
 
-    def get_dumpling
-        self.get_pair each do |p|
-            dumpling.name = p.split(" - ")[0]
-            dumpling.country = p.split(" - ")[1]
+    def make_dumpling
+        self.get_pair.each do |p|
+            dumpling_name = p[0]
+            dumpling = Dumpling.new(dumpling_name, country)
+            if Country.all.detect {|c| c.name == country_name} != nil
+                dumpling.country = Country.all.detect {|c| c.name == country_name}
+            else
+                dumpling.country = Country.new(country_name)
+            end 
         end
     end
 
-    def make_dumpling
+    def get_blurb
 
     end
-
 end
+
+
+
