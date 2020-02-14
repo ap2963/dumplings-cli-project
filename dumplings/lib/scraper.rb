45a63978fd06f7a2a1554d8750be101f3e140fac
agnespark824 <agnespark824@gmail.com>
2020-02-13 20:58:53 -0500

    Fixed formatting on scraper.rb

--- dumplings/lib/scraper.rb
+++ dumplings/lib/scraper.rb
@@ -20,72 +20,69 @@ class Scraper
   end
   
   #scrapes website for information about dumplings
-    def get_article 
-      doc = Nokogiri::HTML(open('https://thecitylane.com/the-best-65-dumplings-around-the-world/'))
-      article = doc.css(".vw-post-content")
-      article
-    end
+  def get_article 
+    doc = Nokogiri::HTML(open('https://thecitylane.com/the-best-65-dumplings-around-the-world/'))
+    article = doc.css(".vw-post-content")
+    article
+  end
 
-	  def get_country_dumpling_pair
-      pair_array = []
-      self.get_article.css("h4").each do |p|
-        pair_array << p.to_s[4...-6]
-			end
-	    @country_dumpling_pair = pair_array.map{| p | p.split(" \u2013 ")}
+  def get_country_dumpling_pair
+    pair_array = []
+    self.get_article.css("h4").each do |p|
+      pair_array << p.to_s[4...-6]
 		end
+    @country_dumpling_pair = pair_array.map{| p | p.split(" \u2013 ")}
+	end
  
-    def create_country_and_dumpling_instances
-      self.get_country_dumpling_pair.each do |p| 
-				country_name = p[1]
-				dumpling_name = p[0]
-				country = Country.find_or_create_by_name(country_name)
-				dumpling = Dumpling.find_or_create_by_name(dumpling_name, country)      
-      end
-		end
-
-    #scrapes wikitable for countries and their regions
-    def get_table
-      doc = Nokogiri::HTML(open("https://meta.wikimedia.org/wiki/List_of_countries_by_regional_classification"))
+  def create_country_and_dumpling_instances
+    self.get_country_dumpling_pair.each do |p| 
+			country_name = p[1]
+			dumpling_name = p[0]
+			country = Country.find_or_create_by_name(country_name)
+			dumpling = Dumpling.find_or_create_by_name(dumpling_name, country)      
     end
+	end
+
+  #scrapes wikitable for countries and their regions
+  def get_table
+    doc = Nokogiri::HTML(open("https://meta.wikimedia.org/wiki/List_of_countries_by_regional_classification"))
+  end
     
-    def table_rows
-      self.get_table.xpath("//tr")
-		end
+  def table_rows
+    self.get_table.xpath("//tr")
+	end
 
-    def table_columns
-      self.table_rows.map do |c|
-        c.xpath("//td").text
-      end
+  def table_columns
+    self.table_rows.map do |c|
+      c.xpath("//td").text
     end
+  end
 
-		#creates an array of arrays that contain a country and its region - [country, region]
-		def get_countries_and_regions
-      full_array = self.table_columns.first.split("\n")
-      until full_array.size == 0
-        full_array.pop
-        @region_country_pair << full_array.pop(2)
-      end
-      @region_country_pair
+	#creates an array of arrays that contain a country and its region - [country, region]
+	def get_countries_and_regions
+    full_array = self.table_columns.first.split("\n")
+    until full_array.size == 0
+      full_array.pop
+      @region_country_pair << full_array.pop(2)
     end
+    @region_country_pair
+  end
 
-		def create_reference_hash
-      @world_regions = self.get_countries_and_regions.map{| a | a[1] }.uniq
-      @world_regions.each{| r | @reference_hash[r.to_sym] = []}
-			self.get_countries_and_regions.each do | a | 
-				@reference_hash[a[1].to_sym] = a[0]
-			end
+	def create_reference_hash
+    @world_regions = self.get_countries_and_regions.map{| a | a[1] }.uniq
+    @world_regions.each{| r | @reference_hash[r.to_sym] = []}
+		self.get_countries_and_regions.each do | a | 
+			@reference_hash[a[1].to_sym] = a[0]
 		end
+	end
 		
-		def create_region_instances
-			Country.all.each do | c |
-				#find region key that has country's name as a string as a value
-				region_name = @reference_hash.key(c.name)
-				Region.find_or_create_by_name(region_name)
-			end
+	def create_region_instances
+		Country.all.each do | c |
+			#find region key that has country's name as a string as a value
+			region_name = @reference_hash.key(c.name)
+			Region.find_or_create_by_name(region_name)
 		end
-
-		def get_blurb
-    end
+	end
 
 end
 
